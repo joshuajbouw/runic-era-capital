8725b561529a26a4175f7040f18ee09fe890bcb9
joshuajbouw <dev@joshuajbouw.com>
2017-03-14 23:10:59 +0700

    Added nodeMailer and BodyParser for Contact form and others.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express 				= require('express'),
 			app 						= express(),
 			port 						= process.env.PORT || 8081,
-			expressLayouts 	= require('express-ejs-layouts');
+			expressLayouts 	= require('express-ejs-layouts'),
+			bodyParser			= require('body-parser'),
+			nodeMailer			= require('nodemailer');
 
 // Static asset location
 app.use(express.static(__dirname + ('/public')));
@@ -9,6 +11,9 @@ app.use(express.static(__dirname + ('/public')));
 // Set EJS as our template engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// Use body parser to grab info from a form. urlencoded is what our form will be processed as.
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set routes
 app.use(require('./app/routes'));
