d6ae9d58725499065e08dcf0fe7c7d846f474878
joshuajbouw <dev@joshuajbouw.com>
2017-03-20 23:19:08 +0700

    Trying path join to fix vids.

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ app.use(session({
 app.use(flash());
 
 // Static asset location
-app.use(express.static(__dirname + ('/public')));
-app.use("/vid", express.static(__dirname + '/public/vid'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Set EJS as our template engine
 app.set('view engine', 'ejs');
