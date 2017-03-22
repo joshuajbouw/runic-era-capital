be8482409186d32b1b740b3a93dcfcb5dc8469d2
joshuajbouw <dev@joshuajbouw.com>
2017-03-22 21:09:02 +0700

    Updated Chinese translations

--- il8n/zh/messages.js
+++ il8n/zh/messages.js
@@ -1,323 +1,323 @@
 ;var json_locale_data = {
    "messages": {
       "": {
-         "Project-Id-Version": " express 4\nReport-Msgid-Bugs-To: JOSHUA J. BOUW <joshua@runicera.capital>\nPOT-Creation-Date: 2017-03-22 15:07+0700\nPO-Revision-Date: 2017-03-22 15:07+0700\nLast-Translator: FULL NAME <EMAIL@ADDRESS>\nLanguage-Team: BLK CHINA\nLanguage: ZH\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n"
+         "MIME-Version": " 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nX-Generator: POEditor.com\nProject-Id-Version: Runic Era Capital Translations\nLanguage: zh-CN\n"
       },
       "Runic Era Capital": [
          null,
-         "‮"
+         "Runic Era 资本 "
       ],
       "Home": [
          null,
-         "‮"
+         "主页"
       ],
       "About Us": [
          null,
-         "‮"
+         "关于我们"
       ],
       "Technology": [
          null,
-         "‮"
+         "技术信息"
       ],
       "Contact": [
          null,
-         "‮"
+         "联系我们"
       ],
       "All Rights Reserved by Runic Era Capital Pvt. Ltd.": [
          null,
-         "‮"
+         "所有权利由Runic Era Capital Pvt. Ltd. 保留。"
       ],
       "A DIFFERENT HEDGE": [
          null,
-         "‮"
+         "一家对冲基金"
       ],
       "A proprietary trading firm specialising in investments made with digital currencies.": [
          null,
-         "‮"
+         "一家专门从事数字货币投资的自营公司。"
       ],
       "Your browser does not support the video tag. I suggest you upgrade your browser.": [
          null,
-         "‮"
+         "您的浏览器不支持此视频播放，请升级您的浏览器。"
       ],
       "IDEAS WORTHWHILE": [
          null,
-         "‮"
+         "点石成金"
       ],
       "16 YEARS OF TRADES": [
          null,
-         "‮"
+         "16年交易经验"
       ],
       "Our": [
          null,
-         "‮"
+         "我们的"
       ],
       "Vision": [
          null,
-         "‮"
+         "蓝图"
       ],
       "To tie the link between new and old systems and provide an opportunity for all to be involved globally around the clock.": [
          null,
-         "‮"
+         "连接新旧系统，并持续为全球参与者提供机会。"
       ],
       "Learn More": [
          null,
-         "‮"
+         "了解更多"
       ],
       "Mission": [
          null,
-         "‮"
+         "任务"
       ],
       "To refine working with the right people and developments to ensure we achieve a high standard of success.": [
          null,
-         "‮"
+         "更好的为战略投资合作伙伴服务，取得更高的回报。"
       ],
       "Apply Now": [
          null,
-         "‮"
+         "现在申请"
       ],
       "To build and use the latest technology to ensure we can accurately spot and take advantage of developing markets and systems.'": [
          null,
-         "‮"
+         "开发并使用最新的技术，以确保我们精准定位市场，利用系统，获取收益。"
       ],
       "Thanks for your interest!": [
          null,
-         "‮"
+         "感谢您的关注！"
       ],
       "For now as we are preparing our operations, we have not made job openings yet but do watch this page.": [
          null,
-         "‮"
+         "我们的业务正在准备中，暂时没有职位空缺，敬请关注。"
       ],
       "However, if you would like to send us your CV regardless, please do drop us a line on our Contact page.": [
          null,
-         "‮"
+         "如果您想投递简历，请在我们的联系页面发送给我们。"
       ],
       "We make tech": [
          null,
-         "‮"
+         "我们开发技术"
       ],
       "Customisable|Flexible|Scalable|Profitable": [
          null,
-         "‮"
+         "可定制|灵活的|可扩展的|可盈利的"
       ],
       "From point of development to deployment...": [
          null,
-         "‮"
+         "从发展到部署…"
       ],
       "BLACKHEDGE": [
          null,
-         "‮"
+         "BLACKHEDGE "
       ],
       "Tying the digital currency world to real tangible markets.": [
          null,
-         "‮"
+         "连接数字货币世界与现实市场。"
       ],
       "Instead of relying on 3rd party websites that don't have the resources to accurately display client details for digital currencies, BlackHedge will aim to bring those private details into our backroom for accurate reporting and record taking for ease of use and ultimate privacy.": [
          null,
-         "‮"
+         "blackhedge将致力于精确使用数据的同时保护个人隐私，取代传统依赖第三方的网站的做法，不需要精确地显示数字货币的客户信息资源。"
       ],
       "Coming soon": [
          null,
-         "‮"
+         "即将到来"
       ],
       "Progress": [
          null,
-         "‮"
+         "进程"
       ],
       "Planning": [
          null,
-         "‮"
+         "计划"
       ],
       "Development": [
          null,
-         "‮"
+         "开发"
       ],
       "Testing": [
          null,
-         "‮"
+         "测试"
       ],
       "Production": [
          null,
-         "‮"
+         "产品"
       ],
       "RUNE LEDGER": [
          null,
-         "‮"
+         "RUNE LEDGER"
       ],
       "Proper digital currency portfolio tools.": [
          null,
-         "‮"
+         "最优数字货币投资组合工具。"
       ],
       "After being discouraged at the rather lack of tools for portfolio tracking for digital currencies, we have set off to build our own. This will come about in phases as we we aim to have a full suite of functional tools that will be comparible to the best.": [
          null,
-         "‮"
+         "在经历了缺乏数字货币的投资组合跟踪工具带来的失望之后，我们开始着手创建自己的跟踪工具。我们会在未来的一段时间内阶段性的完善开发。这套具备全套功能的工具，将是您能在市场上看到的最完美的。"
       ],
       "Get in Touch with Us": [
          null,
-         "‮"
+         "请联系我们"
       ],
       "Successfully sent message!": [
          null,
-         "‮"
+         "消息发送成功！"
       ],
       "Send us an Email": [
          null,
-         "‮"
+         "发送邮件给我们"
       ],
       "Name": [
          null,
-         "‮"
+         "姓名"
       ],
       "Email": [
          null,
-         "‮"
+         "邮箱"
       ],
       "Phone": [
          null,
-         "‮"
+         "电话"
       ],
       "Context": [
          null,
-         "‮"
+         "内容"
       ],
       "Select One": [
          null,
-         "‮"
+         "单选"
       ],
       "General Information": [
          null,
-         "‮"
+         "基本信息"
       ],
       "Investment Enquery": [
          null,
-         "‮"
+         "投资查询"
       ],
       "Employment Opening": [
          null,
-         "‮"
+         "职位空缺"
       ],
       "Other": [
          null,
-         "‮"
+         "其他"
       ],
       "Subject": [
          null,
-         "‮"
+         "主题"
       ],
       "Message": [
          null,
-         "‮"
+         "消息"
       ],
       "Max 500 characters": [
          null,
-         "‮"
+         "最大500字节"
       ],
       "Send Message": [
          null,
-         "‮"
+         "发送消息"
       ],
       "Page Not Found": [
          null,
-         "‮"
+         "页面不存在"
       ],
       "Oops! The Page you were looking for, couldn't be found.": [
          null,
-         "‮"
+         "您查找的页面不存在。"
       ],
       "Try searching for the best match below": [
          null,
-         "‮"
+         "搜索以下相关信息"
       ],
       "Everything you need to know about Runic Era Capital": [
          null,
-         "‮"
+         "有关Runic Era 资本 "
       ],
       "History": [
          null,
-         "‮"
+         "历史"
       ],
       "Background": [
          null,
-         "‮"
+         "背景"
       ],
       "From point of conception and forward...": [
          null,
-         "‮"
+         "概念及前沿…"
       ],
       "Teamwork divides the task": [
          null,
-         "‮"
+         "协作分工"
       ],
       "multiplies the success.": [
          null,
-         "‮"
+         "多倍收益"
       ],
       "From first conception and being established privately in 2015 as Phoenix Capital by two likeminded colleagues and good friends, Sujoy Sikder and Joshua J. Bouw, Phoenix has now been rebranded as Runic Era Capital as of March 2017, after 2 years of success.": [
          null,
-         "‮"
+         "创始人Sujoy Sikder和Joshua J. Bouw，两个志同道合的同事兼好友，从2015年有最初的概念并建立凤凰资本，经过2年的成功经营，凤凰资本于2017年3月更名为 Runic Era 资本。 "
       ],
       "Both Sujoy and Joshua aim to ensure this venture is worthwhile for all those involved. Driven by their common goal to not only work towards developing tools for Runic Era Capital's own use, they aim to also open source the technology by releasing tools for other developers to freely use.": [
          null,
-         "‮"
+         "Sujoy和Joshua在确保所有涉及这个风险是值得的。由他们共同的目标不仅要努力为古代北欧文字的时代资本开发工具的使用,他们的目标也开放源码技术通过释放工具供其他开发者自由使用"
       ],
       "With a successful track record, both recorded and verifiable in the Foreign Exchange markets, the projected goal to develop a team of participants and investors will ensure Runic Era Capital's positioning and competitiveness in the global markets.": [
          null,
-         "‮"
+         ""
       ],
       "Our Four Pillar Philosophy": [
          null,
-         "‮"
+         ""
       ],
       "At Runic Era Capital, we were founded by like-minded individuals whom are driven by our core philosophy.": [
          null,
-         "‮"
+         ""
       ],
       "'Ethical": [
          null,
-         "‮"
+         ""
       ],
       "We will never back anything that can cause gross harm to others.": [
          null,
-         "‮"
+         ""
       ],
       "Profitable": [
          null,
-         "‮"
+         ""
       ],
       "We endeavor to ensure our strategies deliver positive growth.": [
          null,
-         "‮"
+         ""
       ],
       "Risk Manageable": [
          null,
-         "‮"
+         ""
       ],
       "We strive to ensure that the equity is manageable at all times.": [
          null,
-         "‮"
+         ""
       ],
       "Steady Growth": [
          null,
-         "‮"
+         ""
       ],
       "We safeguard to have a progressive incline on our growth, safely.": [
          null,
-         "‮"
+         ""
       ],
       "Assets Under Management": [
          null,
-         "‮"
+         ""
       ],
       "Exchanges Trading On": [
          null,
-         "‮"
+         ""
       ],
       "Average Monthly Growth": [
          null,
-         "‮"
+         ""
       ],
       "Active Clients": [
          null,
-         "‮"
+         ""
       ]
    }
 }
